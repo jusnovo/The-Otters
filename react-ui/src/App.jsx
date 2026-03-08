5552534e1ee8af2a2ffb96ecbc34816bc7f9e3ee
Anshul <anshuldixit589@gmail.com>
2026-03-08 07:14:21 +0000

    Point extension to Render backend

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -179,7 +179,7 @@ export default function App() {
 
       setRawTabs(liveTabData);
 
-      const response = await fetch("http://127.0.0.1:5000/analyse-tabs", {
+      const response = await fetch("https://the-otters.onrender.com/analyse-tabs", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
